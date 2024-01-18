6f75b27ee4570db5cf412fe2a7431b36086a386e
Jacob Karvelis <jak9527@rit.edu>
2024-01-18 18:00:46 -0500

    projectLinkComponent has been made. Will be reusnig it in the projects page

--- personalwebsite/src/pages/Home.js
+++ personalwebsite/src/pages/Home.js
@@ -1,7 +1,9 @@
 import './Home.css'
-import '../images/barcart.jpg'
+// import '../images/barcart.jpg'
 import {BrowserRouter as Router, Routes, Route, Link} from "react-router-dom";
 
+
+// About Summary Components
 function AboutSummary(){
     return (
         <div className='AboutSummaryComponent'>
@@ -36,13 +38,40 @@ function AboutTextComponent(){
     )
 }  
 
+// Project Summary Components
+/**
+ * 
+ * @param {*} props Properties. Used to pass in project name and
+ *                  description, as well as the actual link
+ * @returns 
+ */
+function ProjectLinkComponent(props){
+    return(
+        <a className='ProjectLinkBackground' href={props.url} target="_blank" rel="noreferrer">
+            <img className='ProjectImage' src={props.image} alt='bing'></img>
+            <div className='ProjectTextContainer'>
+                <p className='ProjectTitle'>{props.title}</p>
+                <p className='ProjectDesc'>{props.description}</p>
+            </div>
+        </a>
+    )
+}
+
+
+/**
+ * Page component. This is what is actually displayed
+ */
 function Home(){
     return (
         <div>
             <div className='Homepage'>
-            <AboutSummary />
+                <AboutSummary />
+                <ProjectLinkComponent 
+                title="bingus"
+                description="Gusics Gusics GusicsGusics Gusics Gusics Gusics Gusics"
+                image="https://media.discordapp.net/attachments/1049383116302729368/1196939405965918288/20231219_211126.jpg?ex=65b973b0&is=65a6feb0&hm=7d0cdc4e84f749a0a69e4de9a385074b9f4856f008107f175e03c8caa8866bb2&=&format=webp&width=502&height=670"
+                url="https://www.w3schools.com/html/html_links.asp"/>
             </div>
-            
         </div>
     );
 }
