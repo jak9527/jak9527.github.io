05080ca751afa990974179f986472bbf5dde2bfa
Jacob Karvelis <jak9527@rit.edu>
2024-01-23 12:11:26 -0500

    added construction page

--- personalwebsite/src/App.js
+++ personalwebsite/src/App.js
@@ -7,6 +7,7 @@ import Home from './pages/Home';
 import About from './pages/About';
 import Blog from "./pages/Blog";
 import Projects from "./pages/Projects"
+import Construction from './pages/Construction';
 
 function Navbar () {
   return (
@@ -43,7 +44,8 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path='/about' element={<About />} />
-          <Route path='/blog' element={<Blog />} />
+          {/* <Route path='/blog' element={<Blog />} /> */}
+          <Route path='/blog' element={<Construction />} />
           <Route path='/projects' element={<Projects />} />
         </Routes>
       </div>
