8faa9dfc39306924b6f04bb6aba50a4407b2c0d3
Jacob Karvelis <jak9527@rit.edu>
2024-01-25 14:51:41 -0500

    polished up project summary section

--- personalwebsite/src/pages/Home.js
+++ personalwebsite/src/pages/Home.js
@@ -68,21 +68,21 @@ function ProjectSummaryComponent(){
                 <hr style={{width: "80%", color: "#676367;"}}/>
                 <div style={{display: "flex", width:"100%"}}>
                     <div className='ProjectLinkContainer'>
-                        <ProjectLinkComponent 
+                        <div className='ProjectLinkHolder'><ProjectLinkComponent 
                             title="Slimecrafter"
                             description="A Minecraft mod built in Java for the fabric modloader"
                             image="https://media.discordapp.net/attachments/1049383116302729368/1197992239973027890/controller.png?ex=65bd4837&is=65aad337&hm=60c145aa674c0f1c3c734db13e7f9713ece3091fca5cdffa0068baca8a50a39c&=&format=webp&quality=lossless&width=533&height=670"
-                            url="https://github.com/jak9527/Slimecrafter"/>
-                        <ProjectLinkComponent 
+                            url="https://github.com/jak9527/Slimecrafter"/></div>
+                        <div className='ProjectLinkHolder'><ProjectLinkComponent 
                             title="Mock E-Store"
                             description="An angular based web app that 'Sells' some of RIT's most recognizable monuments"
                             image="https://media.discordapp.net/attachments/1049383116302729368/1198018168657031268/Webapp.png?ex=65bd605d&is=65aaeb5d&hm=95b5bb3977c090cc39b3d67d6e82aa693070eb0d58234db22e2c01a038ec20ff&=&format=webp&quality=lossless&width=522&height=668"
-                            url="https://github.com/jak9527/RIT-Mock-EStore"/>
-                        <ProjectLinkComponent 
+                            url="https://github.com/jak9527/RIT-Mock-EStore"/></div>
+                        <div className='ProjectLinkHolder'><ProjectLinkComponent 
                             title="Puzzle Solver"
                             description="Customizable Puzzle solver, implementation of BFS with pruning for solving puzzles"
                             image="https://media.discordapp.net/attachments/1049383116302729368/1197988456614330388/console.png?ex=65bd44b1&is=65aacfb1&hm=e207eec37058fd4cba14f00117357ed9888094761dc1b437925e98cd0f30c870&=&format=webp&quality=lossless&width=522&height=668"
-                            url="https://github.com/jak9527/Customizable_Game_Puzzle_Solver"/>
+                            url="https://github.com/jak9527/Customizable_Game_Puzzle_Solver"/></div>
                     </div> 
                 </div>
                 <hr style={{width: "80%", color: "#676367;"}}/>
